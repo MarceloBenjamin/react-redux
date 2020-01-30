d3b7b1bea004a9fbb5e2bb777761a4195a668319
Marcelo <marcelo.dfx@gmail.com>
2020-01-30 01:00:53 -0300

    estilizando home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-puma-nrgy-neko-skin-bdp-masculino/06/NWG-0770-006/NWG-0770-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tenis Puma</strong>
+        <span>R$259,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-puma-nrgy-neko-skin-bdp-masculino/06/NWG-0770-006/NWG-0770-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tenis Puma</strong>
+        <span>R$259,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-puma-nrgy-neko-skin-bdp-masculino/06/NWG-0770-006/NWG-0770-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tenis Puma</strong>
+        <span>R$259,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-puma-nrgy-neko-skin-bdp-masculino/06/NWG-0770-006/NWG-0770-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tenis Puma</strong>
+        <span>R$259,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-puma-nrgy-neko-skin-bdp-masculino/06/NWG-0770-006/NWG-0770-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tenis Puma</strong>
+        <span>R$259,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-puma-nrgy-neko-skin-bdp-masculino/06/NWG-0770-006/NWG-0770-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tenis Puma</strong>
+        <span>R$259,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
