afd7bfe195b8848efeeec8e17c8e061a49c12a22
Marcelo <marcelo.dfx@gmail.com>
2020-01-30 23:53:27 -0300

    React-tostify

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
+import 'react-toastify/dist/ReactToastify.css';
 import background from '../assets/images/bg.jpg';
 
 export default createGlobalStyle`
